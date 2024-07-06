18bd6d6311c3c32c93ccba7d727c9b214b49732c
Umer Ghafoor <117443783+umerghafoor@users.noreply.github.com>
2024-07-06 06:20:36 +0500

    chore time sync

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -1,116 +1,145 @@
-#include "DHT.h"
-#include "secrets.h"
+#include <WiFi.h>
 #include <WiFiClientSecure.h>
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
-#include "WiFi.h"
- 
+#include <time.h>
+#include "secrets.h"  // Include your AWS IoT credentials and WiFi credentials here
+#include "DHT.h"
+
 #define DHTPIN 14     // Digital pin connected to the DHT sensor
-#define DHTTYPE DHT11   // DHT 11
- 
-#define AWS_IOT_PUBLISH_TOPIC   "esp32/pub"
-#define AWS_IOT_SUBSCRIBE_TOPIC "esp32/sub"
- 
-float h ;
+#define DHTTYPE DHT11 // DHT 11
+
+#define AWS_IOT_PUBLISH_TOPIC   "Iot-PLant-Monitoring-System/pub"
+#define AWS_IOT_SUBSCRIBE_TOPIC "Iot-PLant-Monitoring-System/sub"
+
+#define TIME_ZONE 10  // Adjust this according to your timezone
+
+float h;
 float t;
- 
+time_t now;
+struct tm timeinfo;
+
 DHT dht(DHTPIN, DHTTYPE);
- 
-WiFiClientSecure net = WiFiClientSecure();
+
+WiFiClientSecure net;
 PubSubClient client(net);
- 
+
+void NTPConnect(void)
+{
+  Serial.print("Setting time using SNTP");
+  configTime(TIME_ZONE * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  now = time(nullptr);
+  while (now < 24 * 3600)
+  {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("done!");
+
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
+}
+
 void connectAWS()
 {
   WiFi.mode(WIFI_STA);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
- 
+
   Serial.println("Connecting to Wi-Fi");
- 
+
   while (WiFi.status() != WL_CONNECTED)
   {
     delay(500);
     Serial.print(".");
   }
- 
+
+  Serial.println("WiFi connected");
+
   // Configure WiFiClientSecure to use the AWS IoT device credentials
   net.setCACert(AWS_CERT_CA);
   net.setCertificate(AWS_CERT_CRT);
   net.setPrivateKey(AWS_CERT_PRIVATE);
- 
+
   // Connect to the MQTT broker on the AWS endpoint we defined earlier
   client.setServer(AWS_IOT_ENDPOINT, 8883);
- 
+
   // Create a message handler
   client.setCallback(messageHandler);
- 
-  Serial.println("Connecting to AWS IOT");
- 
-  while (!client.connect(THINGNAME))
+
+  Serial.println("Connecting to AWS IoT");
+
+  while (!client.connected())
   {
-    Serial.print(".");
-    delay(100);
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect(THINGNAME))
+    {
+      Serial.println("connected");
+      client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
+    }
+    else
+    {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
   }
- 
-  if (!client.connected())
-  {
-    Serial.println("AWS IoT Timeout!");
-    return;
-  }
- 
-  // Subscribe to a topic
-  client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
- 
-  Serial.println("AWS IoT Connected!");
 }
- 
+
 void publishMessage()
 {
   StaticJsonDocument<200> doc;
   doc["humidity"] = h;
   doc["temperature"] = t;
+  doc["TimeStamp"] = asctime(&timeinfo);
+
   char jsonBuffer[512];
-  serializeJson(doc, jsonBuffer); // print to client
- 
+  serializeJson(doc, jsonBuffer);
+
   client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
 }
- 
-void messageHandler(char* topic, byte* payload, unsigned int length)
+
+void messageHandler(char *topic, byte *payload, unsigned int length)
 {
-  Serial.print("incoming: ");
-  Serial.println(topic);
- 
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+
   StaticJsonDocument<200> doc;
-  deserializeJson(doc, payload);
-  const char* message = doc["message"];
+  deserializeJson(doc, payload, length);
+
+  const char *message = doc["message"];
   Serial.println(message);
 }
- 
+
 void setup()
 {
   Serial.begin(115200);
   connectAWS();
   dht.begin();
+  NTPConnect();
 }
- 
+
 void loop()
 {
   h = dht.readHumidity();
   t = dht.readTemperature();
- 
- 
-  if (isnan(h) || isnan(t) )  // Check if any reads failed and exit early (to try again).
+
+  if (isnan(h) || isnan(t))
   {
-    Serial.println(F("Failed to read from DHT sensor!"));
+    Serial.println("Failed to read from DHT sensor!");
     return;
   }
- 
-  Serial.print(F("Humidity: "));
+
+  Serial.print("Humidity: ");
   Serial.print(h);
-  Serial.print(F("%  Temperature: "));
+  Serial.print(" %\tTemperature: ");
   Serial.print(t);
-  Serial.println(F("°C "));
- 
+  Serial.println("°C");
+
   publishMessage();
   client.loop();
-  delay(1000);
+  delay(10000);
 }
